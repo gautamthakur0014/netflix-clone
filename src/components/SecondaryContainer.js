ab9b60578df464b831b3cf498bd22c4ffada1433
Gautam <gautamthakur0014@gmail.com>
2025-06-21 22:41:51 +0530

    add tailwind css

---
+++ src/components/SecondaryContainer.js
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+import lang from "../utils/languageConstants";
+
+const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+  const langKey = useSelector((store) => store.config.lang);
+  return (
+    <div className="-mt-6 ">
+      <MovieList
+        title={lang[langKey].nowPlaying}
+        movies={movies.nowPlayingMovies}
+      />
+      <MovieList title={lang[langKey].popular} movies={movies.popularMovies} />
+      <MovieList
+        title={lang[langKey].topRated}
+        movies={movies.topRatedMovies}
+      />
+      <MovieList
+        title={lang[langKey].upcoming}
+        movies={movies.UpcomingMovies}
+      />
+      {/* <MovieList title={"Comedy"} movies={movies.nowPlayingMovies} /> */}
+    </div>
+  );
+};
+
+export default SecondaryContainer;
