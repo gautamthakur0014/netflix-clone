61cc812471d1adb4ffc9bd6b80a8325c9d9bdf01
Gautam <gautamthakur0014@gmail.com>
2025-06-27 23:51:26 +0530

    shimmer UI

---
+++ src/components/Shimmer/MovieTitleShimmer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./movieTitleShimmer.css";
+
+const MovieTitleShimmer = () => {
+  return (
+    <div class="container">
+      <div class="pic shimmer"></div>
+    </div>
+  );
+}
+
+export default MovieTitleShimmer;
