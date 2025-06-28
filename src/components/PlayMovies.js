3364d0db7e1c23bf64ff8abcec20bc686301472c
Gautam <gautamthakur0014@gmail.com>
2025-06-28 23:44:42 +0530

    add Movie information

--- src/components/PlayMovies.js
+++ src/components/PlayMovies.js
@@ -6,11 +6,12 @@ import MovieList from "./MovieList";
 import useFindPlayMovies from "../hooks/useFindPlayMovies";
 import useSimilarMovies from "../hooks/useSimilarMovies";
 import useRecommendedMovies from "../hooks/useRecommendedMovies";
+import MovieCardShimmer from "./Shimmer/MovieCardShimmer";
 
 const PlayMovies = () => {
   const { Id: movieId } = useParams();
   // console.log("movie id ==== " ,movieId);
-  
+
   const movie = useSelector((state) => state.movies.playMovies);
   const recommendations = useSelector(
     (state) => state.movies.recommendedMovies
@@ -20,13 +21,8 @@ const PlayMovies = () => {
   useRecommendedMovies(movieId);
   useSimilarMovies(movieId);
 
-
-  // useEffect(() => {
-  //   // console.log("Updated similar movies:", similar);
-  // }, [similar]);
-
   if (!movie) {
-    return <p>Loading movie...</p>;
+    return <p></p>;
   }
 
   return (
@@ -37,21 +33,93 @@ const PlayMovies = () => {
         <span className="font-normal">Movie :</span> {movie.title}
       </div>
       <div className="text-white text-sm m-3 md:text-xl md:m-7">
+        <span className="text-sm font-semibold md:text-2xl mb-2 md:mb-4 block">overview :</span>
         {movie.overview}
+      </div>
+      <div className=" text-white m-3 text-sm md:text-base lg:text-lg md:ml-7 space-y-2 mb-4 hide-scrollbar">
+        <div>
+          <span className="font-semibold">Genres:</span>{" "}
+          <span>{movie.genres?.map((genre) => genre.name).join(", ")}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Spoken Languages:</span>{" "}
+          <span>
+            {movie.spoken_languages
+              ?.map((lang) => lang.english_name)
+              .join(", ")}
+          </span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Original Language:</span>{" "}
+          <span className="uppercase">{movie.original_language}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Original Title:</span>{" "}
+          <span>{movie.original_title}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Origin Country:</span>{" "}
+          <span>{movie.origin_country?.join(", ")}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Popularity:</span>{" "}
+          <span>{movie.popularity}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Release Date:</span>{" "}
+          <span>{movie.release_date}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Runtime:</span>{" "}
+          <span>{movie.runtime} mins</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Status:</span>{" "}
+          <span>{movie.status}</span>
+        </div>
+
+        <div>
+          <span className="font-semibold">Vote Average:</span>{" "}
+          <span>⭐ {movie.vote_average}</span>
+        </div>
       </div>
 
       {!recommendations || recommendations.length === 0 ? (
-        <p className="text-white text-4xl ml-7">
-          Loading recommended movies...
-        </p>
+        <div className="flex flex-nowrap overflow-x-scroll hide-scrollbar w-full scroll-smooth">
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+        </div>
       ) : (
         <MovieList title="Recommended" movies={recommendations} />
       )}
 
       {!similar || similar.length === 0 ? (
-        <p className="text-white text-4xl ml-7 mb-7">
-          Loading similar movies...
-        </p>
+        <div className="flex flex-nowrap overflow-x-scroll hide-scrollbar w-full scroll-smooth">
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+          <MovieCardShimmer />
+        </div>
       ) : (
         <MovieList title="Similar Movies" movies={similar} />
       )}
