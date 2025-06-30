781fee45b2bc3d56d393555b304642cdd45b55a2
Gautam <gautamthakur0014@gmail.com>
2025-06-30 20:14:28 +0530

    language const

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,23 @@
+const lang = {
+  en: {
+    nowPlaying: "Now Playing",
+    topRated: "Top Rated",
+    popular: "popular",
+    upcoming: "Upcoming",
+  },
+  hindi: {
+    nowPlaying: "अभी चल रहा है",
+    topRated: "शीर्ष रेटेड",
+    popular: "लोकप्रिय",
+    upcoming: "आगामी",
+  },
+  spanish: {
+    nowPlaying: "En reproducción ",
+    topRated: "Los más valorados",
+    popular: "popular",
+    upcoming: "Próximamente",
+  },
+};
+
+
+export default lang;
