38dc51f54a6fe998a9db008d170d7c6dbbe656d6
Gautam <gautamthakur0014@gmail.com>
2025-06-30 20:03:07 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode> ==> sometime api call happen twice due to this react strict mode but not to woory beacuse it only happens in local not in deployment
+  // react does some extra rendering or our component to check for inconsistency b/w your calls, only happens in dev phase
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
