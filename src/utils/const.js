a86ec6bbad800bee79dbe1925badc03537b38383
Gautam <gautamthakur0014@gmail.com>
2025-06-30 20:08:23 +0530

    add .env

---
+++ src/utils/const.js
@@ -0,0 +1,18 @@
+export const LOGO = "https://i.postimg.cc/JzFrxsRZ/logo.png";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API}`,
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+// console.log(process.env.REACT_APP_TMDB_API);
